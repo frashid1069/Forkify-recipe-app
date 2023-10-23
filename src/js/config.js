5d8b5fffb3feade71a4a60dd3a910761b938a459
Farhan Rashid <141790844+frashid1069@users.noreply.github.com>
2023-10-22 19:30:31 -0600

    Delete src directory

--- src/js/config.js
+++
@@ -1,5 +0,0 @@
-export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
-export const TIMEOUT_SEC = 10;
-export const RES_PER_PAGE = 10;
-export const KEY = 'ce8f7f59-f023-4184-b96a-93d9b337caa2';
-export const MODAL_CLOSE_SEC = 2.5;
